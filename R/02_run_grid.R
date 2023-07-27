41d3b8f16efe2a5de7fc027c5737bda7042f8d0d
Arni Magnusson <thisisarni@gmail.com>
2023-07-28 00:40:54 +1100

    Basic script to run grid models

--- R/02_run_grid.R
+++ R/02_run_grid.R
@@ -2,3 +2,9 @@ library(condor)
 
 session <- ssh_connect("NOUOFPCALC02")
 
+grid.dir <- "m2"
+
+model.dirs <- dir(file.path("../grid", grid.dir), full=TRUE)
+
+for(i in seq_along(model.dirs))
+  try(condor_submit(model.dirs[i]))
