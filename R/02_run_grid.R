73add4d4c8d30fa8e314dd44f8b3af298fc249e0
Arni Magnusson <thisisarni@gmail.com>
2023-07-28 16:19:43 +1100

    Define absolute grid.dir

--- R/02_run_grid.R
+++ R/02_run_grid.R
@@ -3,9 +3,9 @@ library(condor)
 session <- ssh_connect("NOUOFPCALC02")
 options(width=160)
 
-grid.dir <- "m2"
+grid.dir <- "../grid/m2_ini"
 
-model.dirs <- dir(file.path("../grid", grid.dir), full=TRUE)
+model.dirs <- dir(grid.dir, full=TRUE)
 
 for(i in seq_along(model.dirs))
   try(condor_submit(model.dirs[i]))
