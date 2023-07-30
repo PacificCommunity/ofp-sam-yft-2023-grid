4a45305251636f8e4f89d09c520c215a8c00a771
Arni Magnusson <thisisarni@gmail.com>
2023-07-30 19:19:11 +1100

    Script to produce low and high steepness runs

--- R/11a_grid_low_high_steepness.R
+++ R/11a_grid_low_high_steepness.R
@@ -1,17 +1,14 @@
 suppressMessages(library(FLR4MFCL))
 library(tools)  # file_path_sans_ext
 
-from <- "c:/x/yft/mix2/Jitter_14c_m2_s10_a050_h80/Jitter_14c_m2_s10_a050_h80_10"
-steep <- 0.65
-
-steep.int <- round(100 * steep)
-to <- gsub("_h80", paste0("_h", steep.int), from)
-to <- basename(to)
-mix <- as.integer(gsub(".*_m([0-9])_.*", "\\1", from))
+template.dir <- "c:/x/yft/mix2/Jitter_14c_m2_s10_a050_h80/Jitter_14c_m2_s10_a050_h80_10"
 species <- "yft"
+
+mix <- as.integer(gsub(".*_m([0-9])_.*", "\\1", template.dir))
+steep <- c(0.65, 0.95)
 memory <- "small"
 
-top.dir <- paste0("../grid/h", steep.int)
+top.dir <- paste0("../grid/steepness")
 
 age.length.file <- paste0(species, ".age_length")  # from template, not common
 frq.file <- paste0(species, ".frq")
@@ -32,7 +29,6 @@ LLfisheries <-
 ################################################################################
 
 # Read in template par file
-template.dir <- from
 template.parfile <- finalPar(template.dir)
 txt <- readLines(template.parfile)
 first.year <- as.integer(txt[grep("# First year in model", txt) + 1])
@@ -40,48 +36,53 @@ cat("* Studying template", template.parfile, "... ")
 par <- read.MFCLPar(template.parfile, first.yr=first.year)
 cat("done\n")
 
-# Construct model name
-model <- to
-cat(model, fill=TRUE)
-
-# Create directory for grid member
-model.dir <- file.path(top.dir, model)
-if(dir.exists(model.dir))
-  unlink(model.dir, recursive=TRUE)
-dir.create(model.dir, recursive=TRUE)
-file.copy(common.files, model.dir, copy.date=TRUE)
-
-# Modify par object
-# steepness
-steepness(par) <- steep
-# write new par file
-new.parfile <- file.path(model.dir, basename(template.parfile))
-write(par, new.parfile)
-
-# Repair new par file
-# Temporary workaround to solve version conflict between MFCL and FLR4MFCL
-# => This block of code can be removed when FLR4MFCL is updated (Oct 2023)
-# Manually edit the grid parfile, following MFCL 2.2.2.0 format (vsn 1066)
-# We insert two lines of text (first year) into the middle of the parfile
-# But only do this if the first year is not already in the parfile
-txt <- readLines(new.parfile)
-if(!any(grepl("# First year in model", txt)))
+for(k in 1:length(steep))
 {
-  pos2 <- grep("# The grouped_catch_dev_coffs flag", txt)
-  pos1 <- pos2 - 1
-  n <- length(txt)
-  txt <- c(txt[1:pos1], "# First year in model", first.year, txt[pos2:n])
-  writeLines(txt, new.parfile)
+  # Construct model name
+  h.label <- formatC(100*steep, width=2, flag="0")
+  model <- gsub("_h80", paste0("_h", h.label[k]), template.dir)
+  model <- basename(model)
+  cat(model, fill=TRUE)
+
+  # Create directory for grid member
+  model.dir <- file.path(top.dir, model)
+  if(dir.exists(model.dir))
+    unlink(model.dir, recursive=TRUE)
+  dir.create(model.dir, recursive=TRUE)
+  file.copy(common.files, model.dir, copy.date=TRUE)
+
+  # Modify par object
+  # steepness
+  steepness(par) <- steep[k]
+  # write new par file
+  new.parfile <- file.path(model.dir, basename(template.parfile))
+  write(par, new.parfile)
+
+  # Repair new par file
+  # Temporary workaround to solve version conflict between MFCL and FLR4MFCL
+  # => This block of code can be removed when FLR4MFCL is updated (Oct 2023)
+  # Manually edit the grid parfile, following MFCL 2.2.2.0 format (vsn 1066)
+  # We insert two lines of text (first year) into the middle of the parfile
+  # But only do this if the first year is not already in the parfile
+  txt <- readLines(new.parfile)
+  if(!any(grepl("# First year in model", txt)))
+  {
+    pos2 <- grep("# The grouped_catch_dev_coffs flag", txt)
+    pos1 <- pos2 - 1
+    n <- length(txt)
+    txt <- c(txt[1:pos1], "# First year in model", first.year, txt[pos2:n])
+    writeLines(txt, new.parfile)
+  }
+
+  # Use same age data weighting as template model
+  file.copy(file.path(template.dir, age.length.file),
+            file.path(model.dir, age.length.file), copy.date=TRUE)
+
+  # Create generic doitall
+  start.par <- basename(new.parfile)
+  end.par <- paste0(as.integer(file_path_sans_ext(start.par)) + 1, ".par")
+  doitall <- c("#!/bin/sh", "",
+               paste("mfclo64", frq.file, start.par, end.par,
+                     "-switch 2 1 1 10000 1 50 -5"))
+  writeLines(doitall, file.path(model.dir, "doitall.sh"))
 }
-
-# Use same age data weighting as template model
-file.copy(file.path(template.dir, age.length.file),
-          file.path(model.dir, age.length.file), copy.date=TRUE)
-
-# Create generic doitall
-start.par <- basename(new.parfile)
-end.par <- paste0(as.integer(file_path_sans_ext(start.par)) + 1, ".par")
-doitall <- c("#!/bin/sh", "",
-             paste("mfclo64", frq.file, start.par, end.par,
-                   "-switch 2 1 1 10000 1 50 -5"))
-writeLines(doitall, file.path(model.dir, "doitall.sh"))
